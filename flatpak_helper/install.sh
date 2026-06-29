ccccc21afdb241883249b489a5626bd62f74d3e2
Dr. Xiaozhi <jjtseng93@gmail.com>
2026-06-29 20:25:48 +0800

    Always relink bwrap wrapper

--- flatpak_helper/install.sh
+++ flatpak_helper/install.sh
@@ -7,9 +7,10 @@ bwbin=$(command -v bwrap)
 if head -c 4 "$bwbin" | grep -q ELF ; then
   echo Moving the original bwrap to bwrap-real
   mv "$bwbin" "$(dirname "$bwbin")"/bwrap-real
-  chmod +x "$sd"/bwrap_wrapper.sh
-  ln -s "$sd"/bwrap_wrapper.sh "$bwbin"
 else
   echo Already installed!
 fi
 
+chmod +x "$sd"/bwrap_wrapper.sh
+echo Symlinking "$sd"/bwrap_wrapper.sh to "$bwbin"
+ln -sfT "$sd"/bwrap_wrapper.sh "$bwbin"
