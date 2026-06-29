80457af6db57da6a9fea8cfa208a9c34bf3f7b52
Dr. Xiaozhi <jjtseng93@gmail.com>
2026-06-29 12:54:49 +0800

    Konsole runs

--- flatpak_helper/bwrap_wrapper.sh
+++ flatpak_helper/bwrap_wrapper.sh
@@ -4,4 +4,4 @@ sd=$(dirname "$(realpath "$0")")
 
 export LD_PRELOAD="$sd"/libflatpak-preload.so 
 
-exec bwrap-real --bind /data /data "$@"
+exec bwrap-real --bind /dev/ptmx /dev/pts/ptmx --bind /data /data "$@"
